1521b7a2d500d7e5a9a20282b380ca00bdf998a3
Miguel Abaquin <miguel.abaquin@teamsoftware.com>
2026-03-23 04:31:20 +0000

    Added storybook

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,12 @@
 import type { JSX } from 'react'
+import { Container } from '@mui/material'
+
 
 export const NavBar = (): JSX.Element => {
 
   return (
-    <>
-    </>
+    <Container>
+      
+    </Container>
   )
 }
